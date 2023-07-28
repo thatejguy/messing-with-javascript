458f43644d7dc8d7bb6ea20fe8d75ae479f40b73
thatejguy <ejgrotto@gmail.com>
2023-07-28 13:17:00 +1000

    unknown

---
+++ lesson-11/script.js
@@ -0,0 +1,24 @@
+console.log(document.querySelector('button').innerHTML);
+
+document.querySelector('button')
+  .innerHTML = 'Changed';
+
+const buttonElement = document.querySelector('.btn2');
+console.log(buttonElement);
+
+
+/*console.log(document.title);
+document.title = 'Changed';
+
+console.log(document.body);
+console.log(typeof document.body);
+
+console.log(document.body.innerHTML);
+document.body.innerHTML = '<button>Good Job!</button>';
+*/
+
+
+
+/*document.body.innerHTML = 'hello';
+document.title = "Good Job!"
+*/
