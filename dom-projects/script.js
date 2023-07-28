d163e3c68d82abf611d917fa829a99ee9611b8f4
thatejguy <ejgrotto@gmail.com>
2023-07-28 13:23:36 +1000

    new lesson

---
+++ dom-projects/script.js
@@ -0,0 +1,5 @@
+//subscribe button
+const subscribeButton = document.querySelector('.subscribebutton');
+subscribeButton.addEventListener('click', () => {
+  subscribeButton.textContent = 'Subscribed';
+});
