491ee985c7751f06e3c18ffeddbc262fb5fc509d
thatejguy <ejgrotto@gmail.com>
2023-07-31 12:37:13 +1000

    more

--- dom-projects/script.js
+++ dom-projects/script.js
@@ -1,14 +1,26 @@
 //subscribe button
 function subscribe() {
-const subscribeButton = document.querySelector('.subscribebutton');
+  const subscribeButton = document.querySelector('.subscribebutton');
 
-subscribeButton.addEventListener('click', () => {
-  if (subscribeButton.textContent === 'Subscribe') {
+  subscribeButton.addEventListener('click', () => {
+    if (subscribeButton.textContent === 'Subscribe') {
     subscribeButton.textContent = 'Subscribed';
-  } else {
+    } else {
     subscribeButton.textContent = 'Subscribe';
-  }
-});
+    }
+  });
 }
 
 subscribe();
+
+//amazon calculator
+const calculateButton = document.querySelector('.calculatebutton');
+calculateButton.addEventListener('click', () => {
+  calculateTotal();
+});
+
+function calculateTotal() {
+  const inputElement = document.querySelector('.input');
+  console.log(inputElement.value);
+}
+
